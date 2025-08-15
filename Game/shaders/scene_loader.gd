73b8ed2a10d5728d3c3f60eeb7ee7ea418d3c131
Walfalcon <hyperlucas@gmail.com>
2025-08-14 18:09:54 -0700

    Added one more song, made the UI a bit prettier

--- Game/shaders/scene_loader.gd
+++ Game/shaders/scene_loader.gd
@@ -3,7 +3,7 @@ extends Node
 @export var scenes: Array[PackedScene]
 var loaded_scene: MainScene = null
 
-@onready var control_animations: AnimationPlayer = %SongLabelAnimation
+@onready var control_animations: AnimationPlayer = %SceneFadeAnimation
 
 func _ready() -> void:
 	loaded_scene = scenes[0].instantiate()
@@ -12,12 +12,12 @@ func _ready() -> void:
 
 func _on_change_scene(new_scene: int) -> void:
 	get_tree().paused = true
-	control_animations.play("FadeOut")
+	control_animations.play("Fade")
 	await control_animations.animation_finished
 	loaded_scene.queue_free()
 	loaded_scene = scenes[new_scene].instantiate()
 	add_child(loaded_scene)
 	loaded_scene.change_scene.connect(_on_change_scene)
-	control_animations.play_backwards("FadeOut")
+	control_animations.play_backwards("Fade")
 	await control_animations.animation_finished
 	get_tree().paused = false
